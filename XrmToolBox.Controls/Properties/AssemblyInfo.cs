f99acef22d40b63e5d78f1b9658ee175d6afe95e
James Novak <james.novak@gmail.com>
2020-04-09 18:29:40 -0400

    updated version
    damn, missed that
    update to the NUGET package

--- XrmToolBox.Controls/Properties/AssemblyInfo.cs
+++ XrmToolBox.Controls/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The msdyn365 Community")]
 [assembly: AssemblyProduct("XrmToolBox.Controls")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2018.12.1")]
-[assembly: AssemblyFileVersion("1.2018.12.1")]
+[assembly: AssemblyVersion("1.2020.4.9")]
+[assembly: AssemblyFileVersion("1.2020.4.9")]

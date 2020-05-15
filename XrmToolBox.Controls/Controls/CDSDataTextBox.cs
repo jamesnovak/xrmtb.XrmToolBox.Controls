3c3111632b78acb89e678813b95ca654a07b5c48
Jonas Rapp <jonas@jonasr.app>
2020-05-15 12:58:28 +0200

    🚀 Adding CDSDataTextBox control!

---
+++ XrmToolBox.Controls/Controls/CDSDataTextBox.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using xrmtb.XrmToolBox.Controls.Helper;
+
+namespace xrmtb.XrmToolBox.Controls.Controls
+{
+    public partial class CDSDataTextBox : TextBox
+    {
+        #region Private properties
+        private string displayFormat = string.Empty;
+        private bool clickable = false;
+        private string logicalName = null;
+        private Guid id = Guid.Empty;
+        private EntityWrapper entity;
+        private IOrganizationService organizationService;
+        private Font font;
+        #endregion
+
+        #region Public Constructors
+
+        public CDSDataTextBox()
+        {
+            InitializeComponent();
+            font = Font;
+            base.ReadOnly = true;
+            BackColor = SystemColors.Window;
+            Click += HandleClick;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        [Category("Data")]
+        [DisplayName("Record LogicalName")]
+        [Description("LogicalName if the entity type")]
+        public string LogicalName
+        {
+            get
+            {
+                return logicalName;
+            }
+            set
+            {
+                if (value?.Equals(logicalName) == true)
+                {
+                    return;
+                }
+                logicalName = value;
+                id = Guid.Empty;
+                entity = null;
+                Refresh();
+            }
+        }
+
+        [DefaultValue("00000000-0000-0000-0000-000000000000")]
+        [Category("Data")]
+        [DisplayName("Record Id")]
+        [Description("Id of the record. LogicalName must be set before setting the Id.")]
+        public Guid Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value.Equals(id))
+                {
+                    return;
+                }
+                id = string.IsNullOrWhiteSpace(logicalName) ? id = Guid.Empty : value;
+                LoadRecord();
+                Refresh();
+            }
+        }
+
+        [Category("Data")]
+        [DisplayName("Display Format")]
+        [Description("Single attribute from datasource to display for items, or use {{attributename}} syntax freely.")]
+        public string DisplayFormat
+        {
+            get { return displayFormat; }
+            set
+            {
+                if (value?.Equals(displayFormat) == true)
+                {
+                    return;
+                }
+                displayFormat = value;
+                Refresh();
+            }
+        }
+
+        [DefaultValue(false)]
+        [Category("Behavior")]
+        [DisplayName("Record Clickable")]
+        [Description("Displays the record text as a clickable text and fires RecordClick event when clicked")]
+        public bool Clickable
+        {
+            get
+            {
+                return clickable;
+            }
+            set
+            {
+                if (clickable.Equals(value))
+                {
+                    return;
+                }
+                clickable = value;
+                if (clickable)
+                {
+                    ForeColor = SystemColors.HotTrack;
+                    Font = new Font(font, Font.Style | FontStyle.Underline);
+                    Cursor = Cursors.Hand;
+                }
+                else
+                {
+                    ForeColor = SystemColors.ControlText;
+                    Font = font;
+                    Cursor = Cursors.IBeam;
+                }
+            }
+        }
+
+        [ReadOnly(true)]
+        public new bool ReadOnly { get; set; } = true;
+
+        [Browsable(false)]
+        public IOrganizationService OrganizationService
+        {
+            get { return organizationService; }
+            set
+            {
+                organizationService = value;
+                LoadRecord();
+                Refresh();
+            }
+        }
+
+        [Browsable(false)]
+        public EntityReference EntityReference
+        {
+            get
+            {
+                return entity?.Entity?.ToEntityReference();
+            }
+            set
+            {
+                if (value?.LogicalName == logicalName && value?.Id.Equals(Id) == true)
+                {
+                    return;
+                }
+                LogicalName = value?.LogicalName;
+                Id = value?.Id ?? Guid.Empty;
+                Refresh();
+            }
+        }
+
+        [Browsable(false)]
+        public Entity Entity
+        {
+            get
+            {
+                return entity?.Entity;
+            }
+            set
+            {
+                if (entity?.Entity?.Id.Equals(value?.Id) == true)
+                {
+                    return;
+                }
+                entity = value != null ? new EntityWrapper(value, displayFormat, organizationService) : null;
+                logicalName = value?.LogicalName;
+                id = value?.Id ?? Guid.Empty;
+                Refresh();
+            }
+        }
+
+        #endregion Public Properties
+
+        #region Published events
+
+        [Category("CRM")]
+        public event CDSRecordEventHandler RecordClick;
+
+        #endregion Published Events
+
+        #region Private Methods
+
+        private void HandleClick(object sender, EventArgs e)
+        {
+            if (!clickable)
+            {
+                return;
+            }
+            new CDSRecordEventArgs(entity?.Entity, null).OnRecordEvent(this, RecordClick);
+        }
+
+        private void LoadRecord()
+        {
+            if (organizationService != null && !string.IsNullOrWhiteSpace(logicalName) && !Guid.Empty.Equals(Id))
+            {
+                var record = organizationService.Retrieve(logicalName, Id, new ColumnSet(true));
+                entity = new EntityWrapper(record, displayFormat, organizationService);
+            }
+            else
+            {
+                entity = null;
+            }
+        }
+
+        #endregion Private Methods
+
+        #region Public Methods
+
+        public override void Refresh()
+        {
+            if (entity != null && !entity.Format.Equals(displayFormat))
+            {
+                entity.Format = displayFormat;
+            }
+            Text = entity?.ToString();
+            base.Refresh();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ XrmToolBox.Controls/Helper/CDSRecordEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace xrmtb.XrmToolBox.Controls
+{
+    public class CDSRecordEventArgs : EventArgs
+    {
+        public CDSRecordEventArgs(Entity entity, string attribute)
+        {
+            Entity = entity;
+            Attribute = attribute;
+        }
+
+        public Entity Entity { get; }
+
+        public string Attribute { get; }
+
+        public object Value { get { return Entity != null && Entity.Contains(Attribute) ? Entity[Attribute] : null; } }
+
+        public void OnRecordEvent(object sender, CDSRecordEventHandler RecordEventHandler)
+        {
+            RecordEventHandler?.Invoke(sender, this);
+        }
+    }
+
+    public delegate void CDSRecordEventHandler(object sender, CDSRecordEventArgs e);
+}

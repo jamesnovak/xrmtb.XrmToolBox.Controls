7102c3b3ab7522bed762feb9adbb877c57442205
Jonas Rapp <jonas@jonasr.app>
2020-05-18 23:56:51 +0200

    🚀 Adding CDSLookupDialog control

---
+++ XrmToolBox.Controls/Controls/CDSLookupDialog.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace xrmtb.XrmToolBox.Controls.Controls
+{
+    public class CDSLookupDialog : Component
+    {
+        #region Private Fields
+
+        private string[] logicalNames;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// Selected records.
+        /// </summary>
+        [Browsable(false)]
+        public Entity[] Entities { get; private set; }
+
+        /// <summary>
+        /// Selected record, or first selected record is Multiselect=true.
+        /// </summary>
+        [Browsable(false)]
+        public Entity Entity
+        {
+            get
+            {
+                return Entities?.FirstOrDefault();
+            }
+            set
+            {
+                SetRecord(value);
+            }
+        }
+
+        [Description("List of entity logicalnames that shall be available to select from.")]
+        public string[] LogicalNames
+        {
+            get => logicalNames;
+            set
+            {
+                if (value != null && value.Length > 0)
+                {
+                    // Clever way to allow comma separated logicalnames too in the array
+                    logicalNames = 
+                        string.Join(",", value)
+                        .Split(',')
+                        .Select(l => l.Trim())
+                        .Where(l => !string.IsNullOrWhiteSpace(l))
+                        .ToArray();
+                }
+                else
+                {
+                    logicalNames = null;
+                }
+            }
+        }
+
+        [DefaultValue(false)]
+        [Description("Controls whether multiple records can be selected in the dialog.")]
+        public bool Multiselect { get; set; } = false;
+
+        /// <summary>
+        /// IOrganizationService to use when retrieving metadata, views and records.
+        /// </summary>
+        [Browsable(false)]
+        public IOrganizationService Service { get; set; }
+
+        [Description("The string to display in the title bar of the dialog box.")]
+        public string Title { get; set; }
+        #endregion Public Properties
+
+        #region Public Methods
+
+        /// <summary>Shows the form as a modal dialog box.</summary>
+        /// <returns>One of the System.Windows.Forms.DialogResult values.</returns>
+        /// <exception cref="T:System.InvalidOperationException">The form being shown is already visible.-or- The form being shown is disabled.-or-
+        ///     The form being shown is not a top-level window.-or- The form being shown as a
+        ///     dialog box is already a modal form.-or-The current process is not running in
+        ///     user interactive mode (for more information, see System.Windows.Forms.SystemInformation.UserInteractive).</exception>
+        public DialogResult ShowDialog()
+        {
+            return this.ShowDialog(null);
+        }
+
+        /// <summary>Shows the form as a modal dialog box with the specified owner.</summary>
+        /// <param name="owner">Any object that implements System.Windows.Forms.IWin32Window that represents the top-level window that will own the modal dialog box.</param>
+        /// <returns>One of the System.Windows.Forms.DialogResult values.</returns>
+        /// <exception cref="T:System.Exception">This exception can be thrown if Service property is not set.</exception>
+        /// <exception cref="T:System.ArgumentException">The form specified in the owner parameter is the same as the form being shown.</exception>
+        /// <exception cref="T:System.InvalidOperationException">The form being shown is already visible.-or- The form being shown is disabled.-or-
+        ///     The form being shown is not a top-level window.-or- The form being shown as a
+        ///     dialog box is already a modal form.-or-The current process is not running in
+        ///     user interactive mode (for more information, see System.Windows.Forms.SystemInformation.UserInteractive).</exception>
+        public DialogResult ShowDialog(IWin32Window owner)
+        {
+            if (Service == null)
+            {
+                throw new Exception("Service property must be set before calling ShowDialog.");
+            }
+            var title = string.IsNullOrEmpty(Title) ? Multiselect ? "Select Records" : "Select Record" : Title;
+            using (var form = new CDSLookupDialogForm(Service, LogicalNames, Multiselect, title))
+            {
+                var result = form.ShowDialog(owner);
+                Entities = form.GetSelectedRecords();
+                return result;
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private void SetRecord(Entity value)
+        {
+        }
+
+        #endregion Private Methods
+    }
+}
---
+++ XrmToolBox.Controls/Controls/CDSLookupDialogForm.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.ServiceModel;
+using System.Windows.Forms;
+using System.Xml;
+using xrmtb.XrmToolBox.Controls.Helper;
+
+namespace xrmtb.XrmToolBox.Controls.Controls
+{
+    public partial class CDSLookupDialogForm : Form
+    {
+        #region Private Fields
+
+        private Dictionary<string, List<Entity>> entityviews;
+        private string[] logicalNames;
+        private IOrganizationService service;
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public CDSLookupDialogForm(IOrganizationService service, string[] logicalNames, bool multiSelect, string title)
+        {
+            InitializeComponent();
+            SetMulti(multiSelect);
+            Text = title;
+            SetService(service);
+            SetLogicalNames(logicalNames);
+        }
+
+        #endregion Public Constructors
+
+        #region Internal Properties
+
+        internal IOrganizationService Service
+        {
+            get => service;
+            set
+            {
+                service = value;
+                cmbView.OrganizationService = value;
+                gridResults.OrganizationService = value;
+                gridSelection.OrganizationService = value;
+            }
+        }
+
+        #endregion Internal Properties
+
+        #region Internal Methods
+
+        internal Entity[] GetSelectedRecords()
+        {
+            if (gridResults.MultiSelect)
+            {
+                if (gridSelection.GetDataSource<IEnumerable<Entity>>() is IEnumerable<Entity> current)
+                {
+                    return current.ToArray();
+                }
+                return new Entity[] { };
+            }
+            else
+            {
+                return gridResults.SelectedCellRecords?.Take(1).ToArray();
+            }
+        }
+
+        #endregion Internal Methods
+
+        #region Private Methods
+
+        private void LoadData()
+        {
+            if (cmbView.SelectedEntity is Entity view)
+            {
+                txtFilter.Enabled = view.GetAttributeValue<int>(Savedquery.QueryType) == 4;
+                if (!txtFilter.Enabled && !string.IsNullOrWhiteSpace(txtFilter.Text))
+                {
+                    txtFilter.Text = string.Empty;
+                }
+                gridResults.DataSource = service.ExecuteQuickFind(cmbEntity.SelectedItem as string, view, txtFilter.Text);
+                var layout = new XmlDocument();
+                layout.LoadXml(view["layoutxml"].ToString());
+                gridResults.ColumnOrder = String.Join(",", layout.SelectNodes("//cell/@name").OfType<XmlAttribute>().Select(a => a.Value));
+                gridResults.ShowAllColumnsInColumnOrder = true;
+                gridResults.ShowColumnsNotInColumnOrder = false;
+                gridSelection.ColumnOrder = gridResults.ColumnOrder;
+                gridSelection.ShowAllColumnsInColumnOrder = true;
+                gridSelection.ShowColumnsNotInColumnOrder = false;
+            }
+        }
+
+        private void SetLogicalNames(string[] logicalNames)
+        {
+            cmbEntity.Items.Clear();
+            if (logicalNames != null)
+            {
+                cmbEntity.Items.AddRange(logicalNames);
+            }
+            cmbEntity.SelectedIndex = cmbEntity.Items.Count > 0 ? 0 : -1;
+            cmbEntity.Enabled = cmbEntity.Items.Count > 1;
+        }
+
+        private void SetMulti(bool multiSelect)
+        {
+            splitGrids.Panel2Collapsed = !multiSelect;
+            gridResults.MultiSelect = multiSelect;
+        }
+
+        private void SetService(IOrganizationService service)
+        {
+            Service = service;
+            entityviews = new Dictionary<string, List<Entity>>();
+        }
+
+        private void SetViews(string logicalname)
+        {
+            if (!entityviews.ContainsKey(logicalname))
+            {
+                var views = new List<Entity>();
+                if (service.RetrieveViews(logicalname, true) is EntityCollection qfviews)
+                {
+                    views.AddRange(qfviews.Entities);
+                }
+                if (service.RetrieveViews(logicalname, false) is EntityCollection otherviews)
+                {
+                    views.AddRange(otherviews.Entities);
+                }
+                entityviews.Add(logicalname, views);
+            }
+            if (entityviews.ContainsKey(logicalname))
+            {
+                cmbView.DataSource = entityviews[logicalname];
+            }
+        }
+
+        #endregion Private Methods
+
+        #region Private Event Handlers
+
+        private void btnAddSelection_Click(object sender, EventArgs e)
+        {
+            if (gridResults.SelectedRowRecords is IEnumerable<Entity> selected)
+            {
+                var current = GetSelectedRecords().ToList();
+                current.AddRange(selected);
+                gridSelection.DataSource = current.Distinct();
+            }
+        }
+
+        private void btnClearSelection_Click(object sender, EventArgs e)
+        {
+            gridSelection.DataSource = null;
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnRemoveSelection_Click(object sender, EventArgs e)
+        {
+            if (gridSelection.SelectedRowRecords is IEnumerable<Entity> selected)
+            {
+                var current = GetSelectedRecords().ToList();
+                selected.ToList().ForEach(s => current.Remove(s));
+                gridSelection.DataSource = current;
+            }
+        }
+
+        private void cmbEntity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetViews(cmbEntity.SelectedItem as string);
+        }
+
+        private void cmbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void gridResults_SelectionChanged(object sender, EventArgs e)
+        {
+            btnAddSelection.Enabled = gridResults.SelectedRowRecords?.Count() > 0;
+        }
+
+        private void gridSelection_DataSourceChanged(object sender, EventArgs e)
+        {
+            btnClearSelection.Enabled = gridSelection.Rows.Count > 0;
+        }
+
+        private void gridSelection_SelectionChanged(object sender, EventArgs e)
+        {
+            btnRemoveSelection.Enabled = gridSelection.SelectedRowRecords?.Count() > 0;
+        }
+
+        private void txtFilter_Enter(object sender, EventArgs e)
+        {
+            AcceptButton = btnFilter;
+        }
+
+        private void txtFilter_Leave(object sender, EventArgs e)
+        {
+            AcceptButton = btnOk;
+        }
+
+        #endregion Private Event Handlers
+    }
+}

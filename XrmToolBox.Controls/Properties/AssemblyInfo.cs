5a54e1779777778bb59c2222140e7fdc941df450
Jonas Rapp <jonas@jonasr.app>
2021-01-03 16:17:08 +0100

    Updating year in nuspec

--- XrmToolBox.Controls/Properties/AssemblyInfo.cs
+++ XrmToolBox.Controls/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The msdyn365 Community")]
 [assembly: AssemblyProduct("XrmToolBox.Controls")]
-[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyCopyright("Copyright © 2019-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
